40d9235e5bd6e4841c3a4c0a01aee6f153582f90
Svetlana <sve1656@yandex.ru>
2024-01-17 16:55:42 +0300

    changes in test

--- src/test/java/ru/netology/service/VacationServiceTest.java
+++ src/test/java/ru/netology/service/VacationServiceTest.java
@@ -5,19 +5,19 @@ import org.junit.jupiter.api.Test;
 
 public class VacationServiceTest {
     @Test
-    public void test() {
+    public void test () {
         VacationService service = new VacationService();
 
         int expected = 3;
-        int actual = service.calculate(income^ 10_000, expences^ 3_000, threshold^ 20_000);
+        int actual = service.calculate(10_000,  3_000, 20_000);
         Assertions.assertEquals(expected, actual);
     }
     @Test
-    public void test() {
+    public void test2 () {
         VacationService service = new VacationService();
 
-        int expected = 3;
-        int actual = service.calculate(income^ 100_000, expences^ 60_000, threshold^ 150_000);
+        int expected = 2;
+        int actual = service.calculate(100_000,  60_000, 150_000);
         Assertions.assertEquals(expected, actual);
     }
 
